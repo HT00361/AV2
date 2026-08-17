e66ce9c2f02453f8dd9a6cf7667ce318d3d7bea3
Harry Taylor <ht00361@surrey.ac.uk>
2026-08-17 19:22:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* AV2 Field Map — offline app shell.
    Bump CACHE when you edit index.html, or the phone will keep serving the old one. */
-var CACHE = "av2-v16";
+var CACHE = "av2-v17";
 var SHELL = [
   "./",
   "./index.html",
